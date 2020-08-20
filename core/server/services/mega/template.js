cea3ff35a82b37e386ec928aaa44d1183961f8b3
Peter Zimon <zimo@ghost.org>
2020-08-20 18:09:24 +0200

    Refined and simplified template
    no refs.

--- core/server/services/mega/template.js
+++ core/server/services/mega/template.js
@@ -295,58 +295,6 @@ figure blockquote p {
     padding-bottom: 50px;
 }
 
-
-
-
-/* ----- IF THE BROWSER ----- */
-
-.post-title {
-    padding-bottom: 10px;
-    font-size: 42px;
-    line-height: 1.1em;
-    font-weight: 600;
-}
-
-.post-title-link {
-    color: #15212A;
-    display: block;
-    margin-top: 50px;
-}
-
-.post-meta,
-.view-online {
-    padding-bottom: 50px;
-    white-space: nowrap;
-    color: #738a94;
-    font-size: 13px;
-    letter-spacing: 0.2px;
-    text-transform: uppercase;
-}
-    
-.footer-powered {
-    text-align: center;
-}
-    
-.footer-left {
-    color: #738a94;
-    font-size: 13px;
-    padding-top: 7px;
-    width: 50%;
-}
-    
-.footer-right {
-    white-space: nowrap;
-    font-size: 1px;
-    line-height: 1em;
-}
-
-.gh-powered {
-    width: 142px;
-    height: 30px;
-}
-
-/* ----- ELSE ----- */
-
 .post-title {
     padding-bottom: 10px;
     font-size: 42px;
@@ -372,12 +320,6 @@ figure blockquote p {
     text-transform: uppercase;
     text-align: center;
 }
-
-/* ----- ENDIF THE BROWSER ----- */
-
-
-
-
 
 .view-online {
     text-align: right;
@@ -877,6 +819,22 @@ figure blockquote p {
         border-color: #34495e !important;
     }
 }
+
+
+/* ----- IF THE BROWSER ----- */
+    
+.footer-powered {
+    text-align: center;
+    padding-bottom: 40px;
+}
+
+.gh-powered {
+    width: 142px;
+    height: 30px;
+}
+
+/* ----- ENDIF THE BROWSER ----- */
+
 </style>
 </head>
 
@@ -906,7 +864,7 @@ figure blockquote p {
                                 <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
                                 
                                 
-                                <!-- ***** IF NOT THE BROWSER ***** -->
+                                <!-- ***** IF __NOT__ THE BROWSER ***** -->
                                     
                                     <tr>
                                         <td class="site-info" width="100%" align="center">
@@ -923,31 +881,13 @@ figure blockquote p {
                                         </td>
                                     </tr>
 
-                                <!-- ***** ENDIF NOT THE BROWSER ***** --> 
+                                <!-- ***** ENDIF __NOT__ THE BROWSER ***** --> 
 
                                     
                                     <tr>
                                         <td class="post-title"><a href="${post.url}" class="post-title-link">${post.title}</a></td>
                                     </tr>
                                     <tr>
-
-                                    
-                                    <!-- ***** IF THE BROWSER ***** -->
-
-                                        <td>
-                                            <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
-                                                <tr>
-                                                    <td class="post-meta">
-                                                        By ${post.authors} –
-                                                        ${post.published_at} –
-                                                        <a href="${post.url}" class="view-online-link">View online →</a>
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </td>
-
-                                    <!-- ***** ELSE ***** -->
-                                    
                                         <td align="center">
                                             <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
                                                 <tr>
@@ -959,11 +899,6 @@ figure blockquote p {
                                                 </tr>
                                             </table>
                                         </td>
-
-                                    <!-- ***** ENDIF THE BROWSER ***** --> 
-
-
-                                            
                                     </tr>
                                     ${post.feature_image ? `
                                     <tr>
@@ -988,29 +923,19 @@ figure blockquote p {
                             <td class="wrapper" align="center">
                                 <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
                                     <tr>
-                                        
+                                        <td class="footer">${site.title} &copy; ${date.getFullYear()} – <a href="%recipient.unsubscribe_url%">Unsubscribe</a></td>
+                                    </tr>
+
                                     
                                     <!-- ***** IF THE BROWSER ***** -->
 
-                                        <td class="footer" valign="middle">
-                                            <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
-                                                <tr>
-                                                    <td class="footer-left" valign="middle" align="left">${site.title} &copy; ${date.getFullYear()} – <a href="%recipient.unsubscribe_url%">Unsubscribe</a></td>
-                                                    <td class="footer-right" valign="middle" align="right">
-                                                        <a href="https://ghost.org/"><img src="https://static.ghost.org/v3.0.0/images/powered.png" border="0" width="142" height="30" class="gh-powered" alt="Publish with Ghost"></a>
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </td>
-
-                                    <!-- ***** ELSE ***** --> 
-
-                                        <td class="footer">${site.title} &copy; ${date.getFullYear()} – <a href="%recipient.unsubscribe_url%">Unsubscribe</a></td>
-
-                                    <!-- ***** ENDIF THE BROWSER ***** --> 
-
-
+                                    <tr>
+                                        <td class="footer-powered"><a href="https://ghost.org/"><img src="https://static.ghost.org/v3.0.0/images/powered.png" border="0" width="142" height="30" class="gh-powered" alt="Publish with Ghost"></a></td>
                                     </tr>
+
+                                    <!-- ***** ENDIF THE BROWSER ***** -->
+
+
                                 </table>
                             </td>
                         </tr>
